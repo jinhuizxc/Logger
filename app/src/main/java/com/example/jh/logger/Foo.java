10d800d959f48a3ff6bf6b55e48688e8971c04d2
jinhuizxc <1004260403@qq.com>
2017-03-15 18:09:43 +0800

    Initial commit

---
+++ app/src/main/java/com/example/jh/logger/Foo.java
@@ -0,0 +1,13 @@
+package com.example.jh.logger;
+
+import com.orhanobut.logger.Logger;
+
+/**
+ * @author Orhan Obut
+ */
+public class Foo {
+
+    public static void foo(){
+        Logger.d("hello");
+    }
+}
